22a06809dbd5e57aa091434f54f0712cdc85f3e7
Edwin van der Ven <edwinvanderven@gmail.com>
2021-01-18 20:51:03 +0100

    Chapter 2 of the tutorial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'co5+=&cy8paf@rgf-fn#nnq5j!e8gk%h=p%f2p=3u-g&&uv021'
+SECRET_KEY = 'I-do-not-exist'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
